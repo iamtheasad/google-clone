30984a82a285a1aafab073daeb1ef377133a55eb
iamtheasad <iamtheasad@gmail.com>
2020-09-26 10:52:40 +0600

    Updates

---
+++ precache-manifest.3163f56abd80d13d3f8b73bb66f876e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083398c65984793eb3255000bda85654",
+    "url": "/iamtheasad/google-clone.git/index.html"
+  },
+  {
+    "revision": "b3604f263170b7d825bb",
+    "url": "/iamtheasad/google-clone.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0296880c0817379692bb",
+    "url": "/iamtheasad/google-clone.git/static/js/2.0801162a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iamtheasad/google-clone.git/static/js/2.0801162a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3604f263170b7d825bb",
+    "url": "/iamtheasad/google-clone.git/static/js/main.7234ae13.chunk.js"
+  },
+  {
+    "revision": "8487b8681f0f7ed8f030",
+    "url": "/iamtheasad/google-clone.git/static/js/runtime-main.bc0178e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iamtheasad/google-clone.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
