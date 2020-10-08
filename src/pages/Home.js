ee20751e2dd73321b9078d6fd0435676533141c1
iamtheasad <iamtheasad@gmail.com>
2020-10-08 08:06:23 +0600

    header section & logo added

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Home.css';
+import {Link} from "react-router-dom";
+import AppsIcon from '@material-ui/icons/Apps';
+import {Avatar} from '@material-ui/core';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+                <div className="home__headerLeft">
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon/>
+                    <Avatar/>
+                </div>
+            </div>
+            {/*home__header end*/}
+
+            <div className="home__body">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="Logo"/>
+
+                <div className="home__inputContainer">
+
+                </div>
+            </div>
+            {/*Home Body end*/}
+
+        </div>
+    );
+};
+
+export default Home;
